78e5689eaceca7baa1cad867e9c60163ecae3477
Tejas Manohar <me@tejas.io>
2015-08-22 09:20:33 -0500

    basic page

--- js/form.js
+++ js/form.js
@@ -1,8 +1,23 @@
 (function($) {
   $(document).ready(function() {
-    $("#signupForm").submit(function() {
-      console.log('test is firing')
-      window.location.href = "https://www.google.com/";
+    $(".signupForm").submit(function(event) {
+      var formData = {
+        'phoneNumber': $('input[name=phoneNumber]').val()
+      }
+
+      $.ajax({
+        type: 'POST',
+        url: 'https://autolotto-backend-production.herokuapp.com/api/v1/pre/signup',
+        data: formData,
+        dataType: 'json',
+        crossDomain: true,
+        encode: true
+      }).done(function() {
+        window.location.href = "/confirmation";
+      });
+
+      event.preventDefault();
+      window.location.href = "http://a6n2jd5.autolotto.com/confirmation/";
     });
   });
 })(jQuery);
