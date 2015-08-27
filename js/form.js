13c251f0e941dd03c5b218c39d01c479a4dda79c
Tejas Manohar <me@tejas.io>
2015-08-26 23:03:38 -0500

    autolotto confirmation page

--- js/form.js
+++ js/form.js
@@ -13,7 +13,7 @@
         crossDomain: true,
         encode: true
       }).done(function() {
-        window.parent.location.href = "http://autolotto.wpengine.com/confirmation/";
+        window.parent.location.href = "http://autolotto.com/confirmation/";
       }).fail(function() {
         alert('Please enter a valid US phone number!');
       });
