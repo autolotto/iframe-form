e072a77c1773f8caa9bf4f805af2c1135b329d94
Tejas Manohar <me@tejas.io>
2015-11-04 20:36:32 -0600

    search parent window

--- js/form.js
+++ js/form.js
@@ -1,5 +1,5 @@
 function getQuery(q) {
-   var query = window.location.search.substring(1);
+   var query = window.parent.location.search.substring(1);
    var vars = query.split('&');
    for (var i=0; i < vars.length; i++) {
      var pair = vars[i].split('=');
